ffb7facc91247d74c59a3b5aed9275061c7f421d
user <user@users-MacBook-Pro.local>
2020-12-22 14:10:10 -0500

    response react components

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import Toolbar from './Toolbar/Toolbar';
 import MenuBar from './MenuBar/MenuBar';
 import Backdrop from './Backdrop/Backdrop';
+import './products.css';
+
 
 class Products extends Component {
   state = {
@@ -27,13 +29,32 @@ class Products extends Component {
     }
     return (
       <div style={{height: '100%'}}>
-        <Toolbar drawerClickHandler={this.drawerToggleClickHandler} />
-        <MenuBar show={this.state.menuBarOpen} />
-        {backdrop}
-        <div style={{marginTop: '64px'}}>
-          <p>This is Products Page </p>
-        </div>
-        
+          <Toolbar drawerClickHandler={this.drawerToggleClickHandler} />
+          <MenuBar show={this.state.menuBarOpen} />
+          {backdrop}
+          <div class="product-container-3">     
+              <div class="product-item">A1</div>
+              <div class="product-item">A2</div>
+              <div class="product-item">A3</div>       
+              <div class="product-item">A4</div>
+              <div class="product-item">A5</div>
+              <div class="product-item">A6</div>  
+              <div class="product-item">A7</div>
+              <div class="product-item">A8</div>
+              <div class="product-item">A9</div>  
+          </div>  
+          <div class="product-container-2">          
+              <div class="product-item">B1</div>
+              <div class="product-item">B2</div>
+              <div class="product-item">B3</div>
+              <div class="product-item">B4</div>
+              <div class="product-item">B5</div>
+              <div class="product-item">B6</div>
+              <div class="product-item">B7</div>
+              <div class="product-item">B8</div>
+              <div class="product-item">B9</div>
+              <div class="product-item">B10</div>
+          </div>          
       </div>
     );
   }

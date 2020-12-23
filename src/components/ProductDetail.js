6c871dd5df06d8e87b55c0d5776ac916352b9069
user <user@users-MacBook-Pro.local>
2020-12-23 09:48:19 -0500

    response react components

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -2,15 +2,24 @@ import React from 'react';
 
 import Toolbar from './Toolbar/Toolbar';
 import MenuBar from './MenuBar/MenuBar';
+import Backdrop from './Backdrop/Backdrop';
 import './products.css';
 
 
 const ProductDetail = ({match}) => {
+    const [menuBarOpen, setMenuBarOpen] = React.useState(false)
+    const drawerToggleClickHandler =()=>setMenuBarOpen(!menuBarOpen)
+    let backdrop = null;
+
+    if (menuBarOpen) {
+      backdrop = <Backdrop click={this.backdropClickHandler} />
+    }
 
     return (
       <div style={{height: '100%'}}>
-          <Toolbar drawerClickHandler={this.drawerToggleClickHandler} />
-          <MenuBar show={false} />
+          <Toolbar drawerClickHandler={drawerToggleClickHandler} />
+          <MenuBar show={menuBarOpen} />
+          {backdrop}
           
           
           <div style={{width:"100%", fontSize:"20px", marginTop:"280px", textAlign:"center"}}>     

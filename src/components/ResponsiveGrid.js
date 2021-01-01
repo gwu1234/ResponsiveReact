14df9554f1a52ca2aaf581f585747414c5af2148
user <user@users-MacBook-Pro.local>
2021-01-01 10:17:17 -0500

    add responsive button

--- src/components/ResponsiveGrid.js
+++ src/components/ResponsiveGrid.js
@@ -21,25 +21,53 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-
-const ResponsiveMateriaUI = () => {
-  
+const ResponsiveMateriaUI = () => { 
   const [menuBarOpen, setMenuBarOpen] = React.useState(false)
   const drawerToggleClickHandler =()=>setMenuBarOpen(!menuBarOpen)
+  const [screenType, setScreenType] = React.useState('desktop')
+  const [screenSize, setScreenSize] = React.useState(window.innerWidth)
+  const [buttonStyle, setButtonStyle] = React.useState({textAlign:"center", padding:"15px"})
+  // mobile: 500, tablet: 640, laptop: 1024, desktop: 1280,
+  
+  const handleResize = () => {
+    //console.log(window.innerWidth)
+    setScreenSize (window.innerWidth)
+  }
+  window.addEventListener('resize', handleResize);
+
+  React.useEffect (
+    () => {
+       //console.log(`useEffect width = ${screenSize}`)
+       if (screenSize >= 1280 ) {
+          setScreenType('desktop')
+          setButtonStyle ({...buttonStyle, fontSize:"22px", fontWeight:"bold", color:"black", backgroundColor:"white"})
+       } else if (screenSize >=1024) {
+          setScreenType('laptop')
+          setButtonStyle ({...buttonStyle, fontSize:"20px", fontWeight:"bold", color:"black", backgroundColor:"white"})
+       } else if (screenSize >=640) {
+          setScreenType('tablet')
+          setButtonStyle ({...buttonStyle, fontSize:"16px", fontWeight:"normal", color:"white", backgroundColor:"blue", padding:"10px"})
+       } else {
+          setScreenType('mobile')
+          setButtonStyle ({...buttonStyle, fontSize:"14px", fontWeight:"normal", color:"white", backgroundColor:"green", padding:"5px"})
+       }
+       //console.log(buttonStyle)
+    },
+    [screenSize],
+  );
 
   const backdropClickHandler = () => {
       setMenuBarOpen (false);
   };
 
-  
-    let backdrop = null;
-    const classes = useStyles();
+  let backdrop = null;
+  const classes = useStyles();
 
-    if (menuBarOpen) {
+  if (menuBarOpen) {
       backdrop = <Backdrop click={backdropClickHandler} />
-    }
+  }
 
-    return (
+  return (
       <div style={{height: '100%'}}>
           <Toolbar drawerClickHandler={drawerToggleClickHandler} />
           <MenuBar show={menuBarOpen} />
@@ -71,6 +99,9 @@ const ResponsiveMateriaUI = () => {
                   <Paper className={classes.paper}>50% for small, 25% for large screen</Paper>
                 </Grid>
              </Grid>
+             <div style={{marginTop:"20px",  textAlign:"center"}}> 
+                 <button style={buttonStyle}>{screenType}</button>
+             </div>
            </div>
         </div>
     );
